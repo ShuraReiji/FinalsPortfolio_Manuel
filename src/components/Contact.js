61e58f1a31e5f6fe6c9bcd2b81de49b8c4163c4a
ShuraReiji <velblkmore420@gmail.com>
2024-11-21 23:35:17 +0800

    Changed Map and picture"

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -31,25 +31,26 @@ export default function Contact() {
     >
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
         <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-          <iframe
-            width="100%"
-            height="100%"
-            title="map"
-            className="absolute inset-0"
-            frameBorder={0}
-            marginHeight={0}
-            marginWidth={0}
-            style={{ filter: "opacity(0.7)" }}
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d60861.20128511924!2d120.39244185000001!3d17.563512799999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x338e6f821753bae5%3A0x20035e6de330b125!2sVigan%20City%2C%20Ilocos%20Sur!5e0!3m2!1sen!2sph!4v1731081789696!5m2!1sen!2sph"
-          />
+        <iframe
+  width="100%"
+  height="100%"
+  title="map"
+  className="absolute inset-0"
+  frameBorder={0}
+  marginHeight={0}
+  marginWidth={0}
+  style={{ filter: "opacity(0.7)" }}
+  src="https://www.google.com/maps/embed/v1/place?q=Malabon,+Metro+Manila,+Philippines&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+/>
+
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-            <div className="lg:w-1/2 px-6">
+            <div className="lg:w-1/3 px-6">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 ADDRESS
               </h2>
               <p className="mt-1">
-                Vigan City <br />
-                Ilocos Sur
+                Malabon City <br />
+                Metro Manila
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -57,12 +58,12 @@ export default function Contact() {
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                kvbravo@ceu.edu.ph
+                fltm2003414@gmail.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">123-456-7890</p>
+              <p className="leading-relaxed">09957708232</p>
             </div>
           </div>
         </div>
